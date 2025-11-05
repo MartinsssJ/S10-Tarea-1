670894b691c02313e501670ab117f942dce686ca
MartinsssJ <martinjuniorj0@gmail.com>
2025-11-05 12:11:03 -0500

    Avance 2

--- src/tareas10/TareaS10.java
+++ src/tareas10/TareaS10.java
@@ -1,20 +1,45 @@
 package tareas10;
 
+class servicioStock{
+    public boolean validarStock(String producto, int cantidad){
+        return cantidad>0 && cantidad<=10;
+    }
+}
+
+class servicioImpuesto{
+    public double calcularIGV(double subtotal) {
+        return subtotal*0.18;
+    }
+}
+
+class servicioPedido{
+    public void registrarPedido(String cliente, String producto, int cantidad){
+        System.out.println("Pedido registrado correctamente para " + cliente + ".");
+    }
+}
+
 public class TareaS10 {
     public static void main(String[] args) {
+        
+        servicioStock stock = new servicioStock();
+        servicioImpuesto impuesto = new servicioImpuesto();
+        servicioPedido pedido = new servicioPedido();
+        
         String cliente="Juan Pérez";
         String producto="Laptop";
         int cantidad=2;
         double precioUnitario=2500.0;
 
-        if (cantidad<=0){
-            System.out.println("Error: Cantidad inválida.");
+        if (!stock.validarStock(producto, cantidad)){
+            System.out.println("Error: sin stock.");
             return;
         }
 
         double subtotal=precioUnitario*cantidad;
         double igv=subtotal*0.18;
         double total=subtotal+ igv;
+        
+        pedido.registrarPedido(cliente, producto, cantidad);
 
         System.out.println("=== Pedido ===");
         System.out.println("Cliente: " + cliente);
